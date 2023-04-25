5ef41b904c22c35bfce4dae338666931d799b52b
Michael Lanius Friedrich <michaelfriedrich253@gmail.com>
2023-04-25 20:14:31 -0300

    update users status query

--- src/pages/api/getUsersStatus.ts
+++ src/pages/api/getUsersStatus.ts
@@ -11,6 +11,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const usersStatus = await prisma.usersStatus.findMany()
+    const usersStatus = await prisma.usersStatus.findMany({
+        take: 7,
+        orderBy: {
+            updatedAt: 'asc'
+        }
+    })
     res.status(200).json({usersStatus, error: null})
 }
