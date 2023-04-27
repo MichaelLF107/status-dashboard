312ae00b0145435c13ab6652ecaba76e0c1b7e1b
Michael Lanius Friedrich <michaelfriedrich253@gmail.com>
2023-04-27 14:08:47 -0300

    RenderActiveShape componentization

--- src/pages/traffic.tsx
+++ src/pages/traffic.tsx
@@ -3,71 +3,16 @@ import Head from 'next/head'
 import styles from '@/styles/Traffic.module.css'
 import useSwr from 'swr'
 import { formatDate } from '@/utils/utils'
-import { BarChart, Bar, PieChart, Pie, Cell, Sector, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+import { BarChart, Bar, PieChart, Pie, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import { Select, MenuItem } from '@mui/material'
 import Card from '@/components/Card/Card'
+import renderActiveShape from '@/components/RenderActiveShape/RenderActiveShape'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 interface DayInfo {
     name: string
     methodCount: number
-}
-
-interface renderActiveShapeProps {
-    cx: number
-    cy: number
-    midAngle: number
-    innerRadius: number
-    outerRadius: number
-    startAngle: number
-    endAngle: number
-    fill: string
-    payload: any
-    percent: number
-    value: number
-}
-
-const renderActiveShape = ({ cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value }: renderActiveShapeProps) => {
-    const RADIAN = Math.PI / 180
-    const sin = Math.sin(-RADIAN * midAngle)
-    const cos = Math.cos(-RADIAN * midAngle)
-    const sx = cx + (outerRadius + 10) * cos
-    const sy = cy + (outerRadius + 10) * sin
-    const mx = cx + (outerRadius + 30) * cos
-    const my = cy + (outerRadius + 30) * sin
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22
-    const ey = my
-    const textAnchor = cos >= 0 ? 'start' : 'end'
-
-    return (
-        <g>
-            <Sector
-                cx={cx}
-                cy={cy}
-                innerRadius={innerRadius}
-                outerRadius={outerRadius}
-                startAngle={startAngle}
-                endAngle={endAngle}
-                fill={fill}
-            />
-            <Sector
-                cx={cx}
-                cy={cy}
-                startAngle={startAngle}
-                endAngle={endAngle}
-                innerRadius={outerRadius + 6}
-                outerRadius={outerRadius + 10}
-                fill={fill}
-            />
-            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${value}`}</text>
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-                {`(Rate ${(percent * 100).toFixed(2)}%)`}
-            </text>
-        </g>
-    )
 }
 
 export default function TrafficPage(props) {
